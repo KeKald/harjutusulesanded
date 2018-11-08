e657eb6c405627a18751bf72870adb66bc70ab8d
Kermo Kalda <kerps12@gmail.com>
2018-11-08 02:22:58 +0200

    Some improvements

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,14 +1,54 @@
 from random import choice
 
-with open("C:\\Users\\kekalda\\Documents\\Proge\\harjutusulesanded\\Hangman\\words.txt", "r", encoding = "utf-8") as text:
-    words_list = []
-    for line in text:
-        words_list.append(line.replace("\n", ""))
+# Opening file for obtaining word for hangman gane
+# School computer's personal dir is "C:\\Users\\kekalda\\Documents\\Proge\\harjutusulesanded\\Hangman\\words.txt"
+with open("words.txt", "r", encoding="utf-8") as word_file:
+    plain_words_list = []
+    for line in word_file:
+        plain_words_list.append(line.replace("\n", ""))
 
-print(words_list)
+# Debug
+plain_word = choice(plain_words_list)
+print("\n", plain_word)
+print("\n", plain_words_list)
 
-word = choice(words_list)
-print(word)
+# This function makes word suitable for hangman game (showing only on letter and others are replace with underline)
 
-underline = "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ "
-print(word[0:1], underline[0:len(word)])
+def one_lettering(chosen_word):
+
+    one_letter_word = []
+
+    for char in chosen_word:
+
+        if char == chosen_word[0]:
+            one_letter_word.append(char)
+
+        if char != chosen_word[0]:
+            one_letter_word.append(" _ ")
+
+    return "".join(one_letter_word)
+
+
+game_word = one_lettering(plain_word)
+print("\n", game_word)
+
+while True:
+    user_offer = input("Please enter a letter: ")
+
+    lives = 3
+
+    guessed_word = []
+
+    for char in plain_word:
+
+        for user_offer in char:
+
+            if char == user_offer:
+                guessed_word.append("")
+                print("You have guessed a letter!")
+
+            if char != user_offer:
+                lives -= 1
+                print("This not a letter you are looking for!")
+
+    break
