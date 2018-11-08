ab06d7c04eafa2052733c247655cc1ec55173d4b
Kermo Kalda <kerps12@gmail.com>
2018-11-09 01:51:09 +0200

    Final release

--- Hangman/hangman(thonny).py
+++ Hangman/hangman(thonny).py
@@ -2,25 +2,10 @@ from random import choice
 
 # Opening file for obtaining word for hangman gane
 # School computer's personal dir is "C:\\Users\\kekalda\\Documents\\Proge\\harjutusulesanded\\Hangman\\words.txt"
-with open("words.txt", "r", encoding="utf-8") as word_file:
+with open("words2.txt", "r", encoding="utf-8") as word_file:
     plain_words_list = []
     for line in word_file:
         plain_words_list.append(line.replace("\n", ""))
-
-# This function makes word suitable for hangman game (showing only on letter and others are replace with underline)
-def one_lettering(chosen_word):
-
-    one_letter_word = []
-
-    for char in chosen_word:
-
-        if char == chosen_word[0]:
-            one_letter_word.append(char)
-
-        if char != chosen_word[0]:
-            one_letter_word.append(" _ ")
-
-    return "".join(one_letter_word)
 
 # This function checks if there user inserted letter is in a game word
 def letter_control():
@@ -28,11 +13,9 @@ def letter_control():
     global user_offer
 
     if user_offer == plain_word[order]:
-        print("User_offer was in a word")
         return True
     
     if user_offer != plain_word[order]:
-        print("User_offer wasn't in a word")
         return False
 
 # This function checks if there is an 0 or 1 in a array and after that acts acordingly
@@ -43,10 +26,10 @@ def bit_check():
     for order in range(len(bit_word)):
         
         if bit_word[order] == 0:
-
+            
             if letter_control():
                 bit_word[order] = 1
-                
+                       
 def bit_to_word():
     global bit_word
     global plain_word
@@ -59,7 +42,8 @@ def bit_to_word():
         if bit_word[sequence] == 1:
             bit_word[sequence] = plain_word[sequence]
             
-    return print("".join(bit_word))
+    return print("\n", "".join(bit_word))
+
 def word_to_bit():
     global bit_word
     global plain_word
@@ -72,28 +56,92 @@ def word_to_bit():
         if bit_word[sequence] == str():
             bit_word[sequence] = 1
 
-plain_word = choice(plain_words_list)
-game_word = one_lettering(plain_word)
 
-# Debug
-#print("\n", game_word)
-#print("\n", plain_word)
-#print("\n", plain_words_list)
-
-
-bit_word = [1]
-for a in range(len(plain_word) - 1):
-    bit_word.append(int(0))
-
-while True:
-    user_offer = input("Please enter a letter: ")
-    lives = 3
+def init_word_list():
+    global bit_word
+    global plain_word
     
-    # Debug    
-    #print("\n", plain_word)
-    print(bit_word)
+    bit_word = [plain_word[0]]
+    for a in range(len(plain_word)):
+        if a > 1:
+            if plain_word[a] == plain_word[0]:
+                bit_word.append(plain_word[a])
+                
+        if plain_word[a] != plain_word[0]:
+            bit_word.append(int(0))
 
-    bit_check()
-    bit_to_word()
-    word_to_bit()
+def feedback_to_user():
+    global bit_word
+    global user_offer
+    
+    guess = False
+    
+    for sequence in range(len(bit_word)):
+        
+        if user_offer == bit_word[sequence]:
+            
+            guess = True
+                    
+    if guess == True:
+        
+        print("\nYou have guessed a letter!")
+        
+    if guess == False:
+        
+        print("\nTry again!")
+        
+        
+game_state = 0
 
+while True:    
+    # Introduction and explanation for a player
+    if game_state == 0:
+        
+        print("Welcome to the hangman game!"
+              "\n"
+              "You just have to guess all letters from randomly selected word."
+              "\n\n"
+              "If you choose a letter correctly, you are one step close to a victory."
+              "\n"
+              "But"
+              "\n"
+              "If you offer a wrong letter, you will lose one life."
+              "\n\n"
+              "In total you have 3 lives."
+              "\n\n"
+              "------------------------------------------------------")
+        game_state = 1
+    
+    # Initializing first variables and choosing a random word
+    if game_state == 1:
+        
+        lives = 3
+        
+        plain_word = choice(plain_words_list)
+        init_word_list()
+        print("\nYour first word starts with letter {}.\n".format(plain_word[0]))
+        bit_to_word()
+        
+        game_state = 2
+        
+    if game_state == 2:
+        
+        user_offer = input("\nPlease enter your guess: ")
+        
+        if user_offer == bit_word[0]:
+            
+            print("\nThis letter is already exists!")
+            user_offer = input("Please enter letter again: ")
+            
+        if user_offer.isdigit():
+            
+            print("\nYou have entered a number!")
+            user_offer = input("Please enter a letter: ")
+            
+        user_offer = user_offer.lower()
+        
+        word_to_bit()
+        bit_check()
+        bit_to_word()
+        feedback_to_user()
+        word_to_bit()
--- Hangman/hangman(thonny2).py
+++ Hangman/hangman(thonny2).py
@@ -2,88 +2,73 @@ from random import choice
 
 # Opening file for obtaining word for hangman gane
 # School computer's personal dir is "C:\\Users\\kekalda\\Documents\\Proge\\harjutusulesanded\\Hangman\\words.txt"
-with open("words.txt", "r", encoding="utf-8") as word_file:
+with open("words2.txt", "r", encoding="utf-8") as word_file:
     plain_words_list = []
     for line in word_file:
         plain_words_list.append(line.replace("\n", ""))
 
 # This function checks if there user inserted letter is in a game word
-
-
 def letter_control():
     global plain_word
     global user_offer
 
     if user_offer == plain_word[order]:
         return True
-
+    
     if user_offer != plain_word[order]:
         return False
 
 # This function checks if there is an 0 or 1 in a array and after that acts acordingly
-
-
 def bit_check():
     global bit_word
     global order
-
+    
     for order in range(len(bit_word)):
-
+        
         if bit_word[order] == 0:
-
+            
             if letter_control():
                 bit_word[order] = 1
-
-# Transferring 0's and 1's to word
-
-
+                       
 def bit_to_word():
     global bit_word
     global plain_word
-
+    
     for sequence in range(len(bit_word)):
-
+        
         if bit_word[sequence] == 0:
             bit_word[sequence] = " _ "
-
+            
         if bit_word[sequence] == 1:
             bit_word[sequence] = plain_word[sequence]
-
+            
     return print("\n", "".join(bit_word))
-
-# Transferring word to 0's and 1's
-
 
 def word_to_bit():
     global bit_word
     global plain_word
-
+    
     for sequence in range(len(bit_word)):
-
+        
         if bit_word[sequence] == " _ ":
             bit_word[sequence] = 0
-
+            
         if bit_word[sequence] == str():
             bit_word[sequence] = 1
-
-# Creating list starting with 1 and continuning with 0 as long as chosen word word count stops
 
 
 def init_word_list():
     global bit_word
     global plain_word
-
+    
     bit_word = [plain_word[0]]
     for a in range(len(plain_word)):
         if a > 1:
             if plain_word[a] == plain_word[0]:
                 bit_word.append(plain_word[a])
-
+                
         if plain_word[a] != plain_word[0]:
             bit_word.append(int(0))
-
-# Telling how good guess player had
-
 
 def feedback_to_user():
     global bit_word
@@ -91,30 +76,31 @@ def feedback_to_user():
     global lives
 
     guess = False
-
+    
     for sequence in range(len(bit_word)):
-
+        
         if user_offer == bit_word[sequence]:
-
+            
             guess = True
-
+                    
     if guess == True:
-
+        
         print("\nYou have guessed a letter!")
-
+        
     if guess == False:
-
+        
         lives -= 1
         print("\nTry again!")
         print("You have lost a life.")
 
-
+        
+        
 game_state = 0
 
-while True:
+while True:    
     # Introduction and explanation for a player
     if game_state == 0:
-
+        
         print("Welcome to the hangman game!"
               "\n"
               "You just have to guess all letters from randomly selected word."
@@ -129,36 +115,35 @@ while True:
               "\n\n"
               "------------------------------------------------------")
         game_state = 1
-
+    
     # Initializing first variables and choosing a random word
     if game_state == 1:
-
+        
         lives = 3
-
+        
         plain_word = choice(plain_words_list)
         init_word_list()
-        print("\nYour first word starts with letter {}.\n".format(
-            plain_word[0]))
+        print("\nYour first word starts with letter {}.\n".format(plain_word[0]))
         bit_to_word()
-
+        
         game_state = 2
-
-    # Core gameplay
+        
     if game_state == 2:
-
+        
         user_offer = input("\nPlease enter your guess: ")
-        user_offer = user_offer.lower()
-
+        
         if user_offer == bit_word[0]:
-
+            
             print("\nThis letter is already exists!")
             user_offer = input("Please enter letter again: ")
-
+            
         if user_offer.isdigit():
-
+            
             print("\nYou have entered a number!")
             user_offer = input("Please enter a letter: ")
-
+            
+        user_offer = user_offer.lower()
+        
         word_to_bit()
         bit_check()
         bit_to_word()
