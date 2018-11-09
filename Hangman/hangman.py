67898a4ff72e9678ca9366a3cc2aaecaedfc595f
Kermo Kalda <kerps12@gmail.com>
2018-11-09 17:31:21 +0200

    Almost ready

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -32,6 +32,7 @@ def bit_check():
         if bit_word[order] == 0:
 
             if letter_control():
+
                 bit_word[order] = 1
 
 # Transferring 0's and 1's to word
@@ -44,9 +45,11 @@ def bit_to_word():
     for sequence in range(len(bit_word)):
 
         if bit_word[sequence] == 0:
+
             bit_word[sequence] = " _ "
 
         if bit_word[sequence] == 1:
+
             bit_word[sequence] = plain_word[sequence]
 
     return print("\n", "".join(bit_word))
@@ -61,9 +64,11 @@ def word_to_bit():
     for sequence in range(len(bit_word)):
 
         if bit_word[sequence] == " _ ":
+
             bit_word[sequence] = 0
 
         if bit_word[sequence] == str():
+
             bit_word[sequence] = 1
 
 # Creating list starting with 1 and continuning with 0 as long as chosen word word count stops
@@ -74,12 +79,17 @@ def init_word_list():
     global plain_word
 
     bit_word = [plain_word[0]]
+
     for a in range(len(plain_word)):
+
         if a > 1:
+
             if plain_word[a] == plain_word[0]:
+
                 bit_word.append(plain_word[a])
 
         if plain_word[a] != plain_word[0]:
+
             bit_word.append(int(0))
 
 # Telling how good guess player had
@@ -107,7 +117,48 @@ def feedback_to_player():
 
         lives -= 1
         print("\nTry again!")
-        print("You have lost a life.")
+        print("You have lost a life. Only {} lives left!".format(lives))
+
+# Checking if user has guessed all the letters
+
+
+def victory_check():
+    global bit_word
+    global victory
+
+    victory = True
+    for sequence in range(len(bit_word)):
+
+        if bit_word[sequence] == 0:
+
+            victory = False
+
+
+def letter_duplicate_check():
+    global bit_word
+    global user_offer
+
+    for sequence in range(len(bit_word)):
+
+        if user_offer == bit_word[sequence]:
+
+            return True
+
+
+def input_number_check():
+    global user_offer
+
+    if user_offer.isdigit():
+
+        return True
+
+
+def one_letter_input_check():
+    global user_offer
+
+    if len(user_offer) > 1:
+
+        return True
 
 
 game_state = 0
@@ -147,31 +198,50 @@ while True:
     # Core gameplay
     if game_state == 2:
 
-        user_offer = input("\nPlease enter your guess: ")
-        user_offer = user_offer.lower()
+        while True:
+            user_offer = str(input("\nPlease enter your guess: ")).lower()
 
-        if user_offer == bit_word[0]:
+            duplicate = False
+            number = False
+            over_one = False
 
-            print("\nThis letter is already exists!")
-            user_offer = input("Please enter letter again: ")
+            duplicate = letter_duplicate_check()
+            number = input_number_check()
+            over_one = one_letter_input_check()
 
-        if user_offer.isdigit():
+            if duplicate == True:
 
-            print("\nYou have entered a number!")
-            user_offer = input("Please enter a letter: ")
+                print("\nThis letter already exists in the word!")
 
-        if len(user_offer) > 1:
+            if number == True:
 
-            print("Please enter only one letter!")
-            user_offer = input("Please enter a letter again: ")
+                print("\nYour input consists of number(s)!")
 
-        word_to_bit()
-        bit_check()
-        bit_to_word()
-        feedback_to_player()
-        word_to_bit()
+            if over_one == True:
 
-        if lives == 0:
-            print("\nYou have ran out of lives!")
-            print("Maybe next time you are luckier.")
-            break
+                print("\nYou have entered more than 1 character!")
+
+            if duplicate == False and number == False and over_one == False:
+                break
+
+    word_to_bit()
+    bit_check()
+    victory_check()
+    bit_to_word()
+    feedback_to_player()
+    word_to_bit()
+
+    if victory == True:
+
+        print("\nYou have guessed all the letters!")
+        print("Congratulations!")
+        print("You won!")
+
+        break
+
+    if lives == 0:
+
+        print("\nYou have ran out of lives!")
+        print("Maybe next time you are luckier.")
+
+        break
