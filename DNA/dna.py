9a65f121ad18998b7127848f5e9bb392ddca43ae
Kermo Kalda <kerps12@gmail.com>
2018-11-14 01:23:16 +0200

    Little bit overboard

---
+++ DNA/dna.py
@@ -0,0 +1,30 @@
+# Checking if user inputed word is DNA or not
+
+
+def dna_check(input):
+
+    global dna_char
+
+    for char in range(len(input)):
+
+        if input[char] in dna_char:
+
+            return True
+
+# Giving feedback if user inserted word is DNA or not
+
+
+def feedback(a):
+
+    if a == True:
+        print("\nThis word is a DNA!")
+
+    else:
+        print("\nThis word is not a DNA")
+
+
+dna_char = ["a", "t", "c", "g"]
+
+user_input = str(input("\nPlease enter a random word: ")).lower()
+answer = dna_check(user_input)
+feedback(answer)
